16ab3fb5ef7a295971bbaaf8f0a4a3cde532bfba
Phawer <69140238+Phawer-7@users.noreply.github.com>
2022-11-10 23:51:30 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -3,24 +3,25 @@ from aiogram.utils.exceptions import BotBlocked, ChatNotFound
 
 from db import createData, getUser_id
 
-bot = Bot(token="1111111111")
+bot = Bot(token="")
 dp = Dispatcher(bot)
 
-ADMIN_ID = 0000
+ADMIN_USER_ID =
+ADMIN_GROUP_ID =
 
 
 async def sendMessageIfBotCan(message_text: str, user_id: int):
     if not user_id is None:
         try:
             await bot.send_message(chat_id=user_id, text=message_text, parse_mode='HTML')
-            await bot.send_message(chat_id=ADMIN_ID, text='Сообщение было отправлено')
+            await bot.send_message(chat_id=ADMIN_GROUP_ID, text='Сообщение было отправлено.')
         except BotBlocked:
-            await bot.send_message(chat_id=ADMIN_ID, text='Пользователь заблокировал бота.')
+            await bot.send_message(chat_id=ADMIN_GROUP_ID, text='Пользователь заблокировал бота.')
         except ChatNotFound:
-            await bot.send_message(chat_id=ADMIN_ID, text="Ты не можешь писать пользователю который боту еще ничего не "
-                                                          "писал")
+            await bot.send_message(chat_id=ADMIN_GROUP_ID, text="Ты не можешь писать пользователю который боту еще "
+                                                                "ничего не писал.")
     else:
-        await bot.send_message(chat_id=ADMIN_ID, text="Error. Используй /send id текст")
+        await bot.send_message(chat_id=ADMIN_GROUP_ID, text="Error. Используй /send id текст")
 
 
 @dp.message_handler(commands=['start'])
@@ -28,36 +29,33 @@ async def greeting(message: types.Message):
     if message.chat.type == 'private':
         await message.answer('Чтобы зарегать свою пару заполните анкету - \n1. Юзы двух пар.\n2. Смайл пары')
     else:
-        await message.answer('Я работаю только в личных чатах.')
+        if not message.from_user.id == ADMIN_USER_ID:
+            await message.answer('Я работаю только в личных чатах.')
 
 
 @dp.message_handler(commands=['send', 'отправить'], commands_prefix="!?/\.#$")
 async def sendMessageToUser(message: types.Message):
-    if message.from_user.id == ADMIN_ID:
+    if message.from_user.id == ADMIN_USER_ID:
         try:
             userid = int(message.text.split()[1])
             text = " ".join(message.text.split()[2:])
 
             await sendMessageIfBotCan(message_text=text, user_id=userid)
-
-        except IndexError:
-            await message.answer('<code>/send id_пользователя текст</code>', parse_mode='HTML')
-        except ValueError:
+        except IndexError or ValueError:
             await message.answer('<code>/send id_пользователя текст</code>', parse_mode='HTML')
 
 
 @dp.message_handler(content_types=['text'])
 async def wrote_text(message: types.Message):
     if message.chat.type == 'private':
-        if not message.from_user.id == ADMIN_ID:
-            msgId = await bot.send_message(ADMIN_ID, text=message.text)
+        if not message.from_user.id == ADMIN_USER_ID:
+            msgId = await bot.send_message(ADMIN_GROUP_ID, text=message.text)
             createData(text=message.text, message_id=msgId['message_id'], user_id=message.from_user.id)
-        else:
-            if message.reply_to_message:
+    else:
+        if message.reply_to_message:
+            if message.from_user.id == ADMIN_USER_ID:
                 id = getUser_id(message_id=message.reply_to_message.message_id)
                 await sendMessageIfBotCan(message_text=message.text, user_id=id)
-            else:
-                await message.answer('Нужно отвечать в ответ на сообщение')
 
 
 if __name__ == "__main__":
